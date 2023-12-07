084253e9b0a2cbe814c9fe90150b9275209b59c4
Broccoli <90281858+koma123451@users.noreply.github.com>
2023-12-07 17:21:56 -0500

    added error message if user enter wrong name

--- index.js
+++ index.js
@@ -8,32 +8,41 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon")
 async function checkWeather(city){
     const response = await fetch(apiUrl + city+`&appid=${apiKey}`);
-    var data = await response.json();
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block"
+        document.querySelector(".weather").style.display = "none"
+    }else{
+        var data = await response.json();
 
-    console.log(data)
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) +"°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
-    document.querySelector(".wind").innerHTML = data.wind.speed+ "km/h";
-
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "images/clouds.png";
+    
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) +"°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
+        document.querySelector(".wind").innerHTML = data.wind.speed+ "km/h";
+    
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "images/clear.png"
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png"
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png"
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "images/mist.png"
+        }
+        
     }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "images/clear.png"
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "images/rain.png"
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "images/drizzle.png"
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "images/mist.png"
-    }
+   
+  
+    
 
 }
 searchBtn.addEventListener("click",()=>{
     checkWeather(searchBox.value);
+    document.querySelector(".weather").style.display = "block";
 })
-checkWeather("toronto");
